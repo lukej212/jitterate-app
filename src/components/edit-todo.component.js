d1fe45b716b1eff791ab9e8254be7a46e7d8caef
Luke.Johnson <Luke.Johnson@dmp.com>
2019-04-01 12:52:35 -0500

    Implement weather api

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -1,28 +1,25 @@
 import React, {Component} from 'react';
+import '../App.css';
+import nap from '../giphy.gif';
 
 export default class EditTodo extends Component {
     render() {
         return (
             <div class="container">
                 <div class="right-div right-text">
-                    Delenit et vero vero nonummy
-                    ad amet, vulputate eum consequat.
+                    <img src={nap} style={{width:'100px', height:'100px'}} />
                 </div>
                 <div class="right-div right-text">
-                    Delenit et vero vero nonummy
-                    ad amet, vulputate eum consequat.
+                    <img src={nap} style={{width:'100px', height:'100px'}} />
                 </div>
                 <div class="right-div right-text">
-                    Delenit et vero vero nonummy
-                    ad amet, vulputate eum consequat.
+                    <img src={nap} style={{width:'100px', height:'100px'}} />
                 </div>
                 <div class="right-div right-text">
-                    Delenit et vero vero nonummy
-                    ad amet, vulputate eum consequat.
+                    <img src={nap} style={{width:'100px', height:'100px'}} />
                 </div>
                 <div class="right-div right-text">
-                    Delenit et vero vero nonummy
-                    ad amet, vulputate eum consequat.
+                    <img src={nap} style={{width:'100px', height:'100px'}} />
                 </div>
             </div>
         );
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,26 +1,38 @@
 import React, {Component} from 'react';
 import '../App.css';
-import nap from '../giphy.gif';
 
-export default class TodosList extends Component {
+export default class CurrentWeather extends Component {
+    constructor() {
+        super();
+        this.state = {
+            forecast: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://api.apixu.com/v1/forecast.json?key=513d8003c8b348f1a2461629162106&q=65804&days=10')
+        .then(results => {
+            return results.json();
+        }).then(data => {
+            console.log(data);
+            let forecast = data.forecast.forecastday.map((element) => {
+                console.log(element.day.condition.icon);
+                return(
+                    <div class="right-div right-text">
+                        <div class="center-text">{element.date}</div>
+                        <img src={element.day.condition.icon} style={{width:'100px', height:'100px'}} />
+                        <div class="center-text">{`high ${element.day.maxtemp_f}`}</div> 
+                    </div>
+                )
+            })
+            this.setState({forecast: forecast});
+        })
+    }
+
     render() {
         return (
             <div class="container">
-                <div class="right-div right-text">
-                    <img src={nap} style={{width:'100px', height:'100px'}} />
-                </div>
-                <div class="right-div right-text">
-                    <img src={nap} style={{width:'100px', height:'100px'}} />
-                </div>
-                <div class="right-div right-text">
-                    <img src={nap} style={{width:'100px', height:'100px'}} />
-                </div>
-                <div class="right-div right-text">
-                    <img src={nap} style={{width:'100px', height:'100px'}} />
-                </div>
-                <div class="right-div right-text">
-                    <img src={nap} style={{width:'100px', height:'100px'}} />
-                </div>
+                {this.state.forecast}
             </div>
         );
     }

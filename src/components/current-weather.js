ed08e3ce02289fc783ddb9369138b4566b2d8b24
lej212 <edward212@live.missouristate.edu>
2019-05-14 12:46:09 -0500

    integrate animations

--- src/components/current-weather.js
+++ src/components/current-weather.js
@@ -3,9 +3,13 @@ import '../App.css';
 import sketchVars from '../sketchVars.json';
 import P5Wrapper from 'react-p5-wrapper';
 import {Card} from 'react-bootstrap';
-import sketch from '../sketches/sketch';
-const ROOT_ADDRESS = `https://api.apixu.com/v1/forecast.json?key=${process.env.WEATHER_KEY}&q=`;
-const GOOG_ADDRESS = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOG_KEY}&latlng=`;
+import rain from '../sketches/rain';
+import sunny from '../sketches/sunny';
+import cloudy from '../sketches/cloudy';
+import pcloudy from '../sketches/pcloudy';
+import storms from '../sketches/storms';
+const ROOT_ADDRESS = `https://api.apixu.com/v1/forecast.json?key=5354360a83f54cd69d2163637190104&q=`;
+const GOOG_ADDRESS = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBY1S0EAcqxiaEBg4hGtEwEuIr83-0ncGM&latlng=`;
 
 export default class CurrentWeather extends Component {
     constructor() {
@@ -52,9 +56,22 @@ export default class CurrentWeather extends Component {
                 console.log(sketchVars.wind_dir);
                 console.log(sketchVars.wind_mph);
                 console.log(sketchVars.humidity);
+                let weathercode = data.current.condition.code;
+                let weatherAnim;
+                if(weathercode === 1000)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={sunny} />
+                else if(weathercode === 1003)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={pcloudy} />
+                else if(weathercode === 1006 || weathercode === 1009 || weathercode === 1135 || weathercode === 1147)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={cloudy} />
+                else if(weathercode === 1087 || weathercode === 1195 || weathercode === 1243 || weathercode === 1246 
+                    || weathercode === 1276 || weathercode === 1279 || weathercode === 1282)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={storms} />
+                else 
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={rain} />
                 let forecast = (
                     <Card className="right-div right-text" style={{ width: '18rem' }}>
-                        <P5Wrapper class="center-block" variant="top" sketch={sketch} />
+                        {weatherAnim}
                         <Card.Body>
                             <Card.Title>{locationName}</Card.Title>
                             <div>
--- src/components/forecast.js
+++ src/components/forecast.js
@@ -2,8 +2,13 @@ import React, {Component} from 'react';
 import '../App.css';
 import P5Wrapper from 'react-p5-wrapper';
 import {Card, Image} from 'react-bootstrap';
-import sketch from '../sketches/sketch';
-const ROOT_ADDRESS = `https://api.apixu.com/v1/forecast.json?days=5&key=${process.env.WEATHER_KEY}&q=`;
+import sketch from '../sketches/weatherSketch';
+import rain from '../sketches/rain';
+import sunny from '../sketches/sunny';
+import cloudy from '../sketches/cloudy';
+import pcloudy from '../sketches/pcloudy';
+import storms from '../sketches/storms';
+const ROOT_ADDRESS = `https://api.apixu.com/v1/forecast.json?days=5&key=5354360a83f54cd69d2163637190104&q=`;
 
 export default class Forecast extends Component {
     constructor() {
@@ -24,9 +29,22 @@ export default class Forecast extends Component {
             .then(data => {
             let forecast =  data.forecast.forecastday.map((element, index) => {
                 let dayOfWeek = dayNum + index <= 6 ? this.weekdays[dayNum + index] : this.weekdays[Math.abs(dayNum - index -1)];
+                let weathercode = element.day.condition.code;
+                let weatherAnim;
+                if(weathercode === 1000)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={sunny} />
+                else if(weathercode === 1003)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={pcloudy} />
+                else if(weathercode === 1006 || weathercode === 1009 || weathercode === 1135 || weathercode === 1147)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={cloudy} />
+                else if(weathercode === 1087 || weathercode === 1195 || weathercode === 1243 || weathercode === 1246 
+                    || weathercode === 1276 || weathercode === 1279 || weathercode === 1282)
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={storms} />
+                else 
+                    weatherAnim = <P5Wrapper class="center-block" variant="top" sketch={rain} />
                 return(
                     <Card key={index} className="right-div right-text" style={{ width: '18rem' }}>
-                    <P5Wrapper class="center-block" variant="top" sketch={sketch} />
+                    {weatherAnim}
                     <Card.Body>
                         <Card.Title>{dayOfWeek}</Card.Title>
                         <Card.Text>
